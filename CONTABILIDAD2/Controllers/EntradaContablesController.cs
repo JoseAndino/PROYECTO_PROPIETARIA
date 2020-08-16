977407c6eb1785603f6c09b72b08fd1ae4d3975a
JOSE ANTONIO ANDINO PEREZ <20180543@unapec.edu.do>
2020-08-16 19:53:18 -0400

    SEGUNDO CAMBIO LLAMADA API entra contable pero no funciona bien

---
+++ CONTABILIDAD2/Controllers/EntradaContablesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using CONTABILIDAD2.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CONTABILIDAD2.Controllers
+{
+
+    public class EntradaContablesController : Controller
+    {
+
+        string Baseurl = "http://intcontabilidad.azurewebsites.net/";
+        // GET: EntradaContables
+        public async Task<ActionResult> Index()
+        {
+            List<EntradaContable> EntradaContables = new List<EntradaContable>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //llamada todos las entradas contables usando el HttpClient
+                HttpResponseMessage Res = await client.GetAsync("api/EntradaContables/dummy");
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Si Res= true entra y asigna los datos
+                    var EntradaResponse = Res.Content.ReadAsStringAsync().Result;
+                    //Deserializar el api y almacena los datos
+
+                     EntradaContables = JsonConvert.DeserializeObject<List<EntradaContable>>(EntradaResponse);
+
+                }
+                //Muestra la lista de todos las entradas contables
+
+            }
+            return View(EntradaContables);
+
+
+           
+        }
+    }
+}
---
+++ CONTABILIDAD2/Models/EntradaContable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CONTABILIDAD2.Models
+{
+    public class EntradaContable
+    {
+        public int id { get; set; }
+        public string descripcion { get; set; }
+        public DateTime fecha { get; set; }
+        public double monto { get; set; }
+        public int auxiliarId { get; set; }
+        public int monedaId { get; set; }
+        public bool estado { get; set; }
+    }
+}
